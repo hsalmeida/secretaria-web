3c75b3f57ff1dda4dd83b487c16b49fab0569f15
hsalmeida <hsalmeida@gmail.com>
2016-05-04 23:44:54 -0300

    home

--- js/components/login/login.js
+++ js/components/login/login.js
@@ -1,41 +1,41 @@
 angular.module('secretariaweb').controller('LoginController', ['$scope', '$rootScope', '$state', '$cookies',
     'Usuario',
-    function($scope, $rootScope, $state, $cookies, Usuario, Grupo){
+    function ($scope, $rootScope, $state, $cookies, Usuario, Grupo) {
         $rootScope.bodybg = {
             background: '#db4437 url(img/core/cover.png) no-repeat fixed center'
         };
 
         var angularCookieUsrObj = $cookies.getObject('secretariaWebCookieKeyUsrObj');
-        if(angularCookieUsrObj) {
+        if (angularCookieUsrObj) {
             $rootScope.currentUser = angularCookieUsrObj;
         }
 
-        if($rootScope.currentUser && $rootScope.currentUser.remember) {
+        if ($rootScope.currentUser && $rootScope.currentUser.remember) {
             $scope._email = $rootScope.currentUser.email;
             $scope._remem = $rootScope.currentUser.remember;
         }
 
-        function assignCurrentUser (user) {
+        function assignCurrentUser(user) {
             $rootScope.currentUser = user;
             return user;
         }
 
-        $scope.submit = function(email, password, rememberMe) {
+        $scope.submit = function (email, password, rememberMe) {
             var query = {
-                "email" : email,
-                "password" : password,
-                "secretariaweb" : true
+                "email": email,
+                "password": password,
+                "secretariaweb": true
             };
-            Usuario.query(query).then(function(user){
+            Usuario.query(query).then(function (user) {
                 $scope.notFound = false;
-                if(user[0]){
+                if (user[0]) {
                     var curUser = user[0];
 
                     curUser.remember = rememberMe;
                     assignCurrentUser(curUser);
-                    if(rememberMe) {
-                        $cookies.putObject('secretariaWebCookieKeyUsrObj', curUser);
-                    }
+                    //if(rememberMe) {
+                    $cookies.putObject('secretariaWebCookieKeyUsrObj', curUser);
+                    //}
 
                     $state.go('home');
 
